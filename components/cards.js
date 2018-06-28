49baebe3fe632ba308828fc586b90ed9ce594d9f
Diego Phares <pharesdiego@gmail.com>
2018-06-28 15:42:14 -0400

    less verbosity

--- components/cards.js
+++ components/cards.js
@@ -1,20 +1,7 @@
 import Card from './card.js';
 
-const getUsefulRounds = rounds => rounds.map(({ name, matches }) => ({
-  name,
-  matches: getUsefulDataFromMatches(matches)
-}));
-
-const getUsefulDataFromMatches = matches => matches.map(match => ({
-  date: match.date,
-  score1: match.score1,
-  score2: match.score2,
-  team1: match.team1,
-  team2: match.team2
-}));
-
 const generateCards = data => data.map(obj => Card(obj));
 
-const Cards = data => generateCards(getUsefulRounds(data.rounds));
+const Cards = data => generateCards(data.rounds);
 
 export default Cards;
